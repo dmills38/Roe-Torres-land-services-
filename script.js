0a7b9b7b0ab862657053249da216d95fca99acdd
Daniel Mills <danieljmills8808@gmail.com>
2026-05-13 19:01:35 -0600

    fixed js issue that made clicks broken on mobile

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const services = document.querySelectorAll(".services li");
+document.addEventListener("DOMContentLoaded", () => {
+    const services = document.querySelectorAll(".services li");
 
-services.forEach(service => {
-    service.addEventListener("click", () => {
+    services.forEach(service => {
+        service.addEventListener("click", () => {
 
-        // close all other services first
-        services.forEach(item => {
-            if (item !== service) {
-                item.classList.remove("active");
-            }
+            // close all others
+            services.forEach(item => {
+                if (item !== service) {
+                    item.classList.remove("active");
+                }
+            });
+
+            // toggle current
+            service.classList.toggle("active");
         });
-
-        // toggle clicked service
-        service.classList.toggle("active");
     });
 });
