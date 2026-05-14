d4101b715723e7286559c16c48794c3c8031b3d8
Daniel Mills <danieljmills8808@gmail.com>
2026-05-13 18:56:45 -0600

    fixed mobile service bug where clicks were delayed

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const services = document.querySelectorAll(".services li");
 
 services.forEach(service => {
     service.addEventListener("click", () => {
+
+        // close all other services first
+        services.forEach(item => {
+            if (item !== service) {
+                item.classList.remove("active");
+            }
+        });
+
+        // toggle clicked service
         service.classList.toggle("active");
     });
 });
